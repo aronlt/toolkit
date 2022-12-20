e0fc363416af3f708bc200ea1578d51c869378d0
lintong.0825 <lintong.0825@bytedance.com>
2022-12-20 17:05:52 +0800

    enum

--- demo/enum.go
+++ demo/enum.go
@@ -1,51 +1,10 @@
 package demo
 
-import (
-	"database/sql/driver"
-	"encoding/json"
-	"fmt"
-
-	"github.com/pkg/errors"
-)
-
-type LeaveType string
+type LeaveType = string
 
 const (
 	AnnualLeave LeaveType = "AnnualLeave"
-	Sick                  = "Sick"
-	BankHoliday           = "BankHoliday"
-	Other                 = "Other"
+	Sick        LeaveType = "Sick"
+	BankHoliday LeaveType = "BankHoliday"
+	Other       LeaveType = "Other"
 )
-
-func (lt *LeaveType) UnmarshalJSON(b []byte) error {
-	var s string
-	err := json.Unmarshal(b, &s)
-	if err != nil {
-		return err
-	}
-	leaveType := LeaveType(s)
-	switch leaveType {
-	case AnnualLeave, Sick, BankHoliday, Other:
-		*lt = leaveType
-		return nil
-	}
-	return errors.New("Invalid leave type")
-}
-
-// MarshalJSON implements json.Marshaler.
-func (lt LeaveType) MarshalJSON() ([]byte, error) {
-	s := string(lt)
-	return json.Marshal(s)
-}
-
-func (lt *LeaveType) Scan(value interface{}) error {
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
-	}
-	return lt.UnmarshalJSON(bytes)
-}
-
-func (lt LeaveType) Value() (driver.Value, error) {
-	return lt.MarshalJSON()
-}
