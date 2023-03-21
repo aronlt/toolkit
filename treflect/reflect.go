c3b5b022d1b48a634efbf83fcf864ab78f42ce87
lintong <lintong.0825@bytedance.com>
2023-03-21 18:18:52 +0800

    feat: reflect, to any map

--- treflect/reflect.go
+++ treflect/reflect.go
@@ -3,6 +3,7 @@ package treflect
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 func SetField(item interface{}, fieldName string, value interface{}) error {
@@ -36,4 +37,33 @@ func GetFieldValue(item interface{}, fieldName string) (reflect.Value, error) {
 		return f, fmt.Errorf("can't find field name")
 	}
 	return f, nil
+}
+
+// ToAnyMap 把任意数据转换为json字符串形式的任意map
+func ToAnyMap(item interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	r := reflect.TypeOf(item)
+	if r.Kind() == reflect.Pointer {
+		r = r.Elem()
+	}
+	v := reflect.ValueOf(item)
+	if v.Kind() == reflect.Pointer {
+		v = v.Elem()
+	}
+	n := r.NumField()
+	for i := 0; i < n; i++ {
+		field := r.FieldByIndex([]int{i})
+		value := v.FieldByIndex([]int{i})
+		tag := field.Tag.Get("json")
+		if idx := strings.Index(tag, ","); idx != -1 {
+			tag = tag[:idx]
+		}
+		tag = strings.TrimSpace(tag)
+		if value.Kind() == reflect.Pointer {
+			result[tag] = value.Elem().Interface()
+		} else {
+			result[tag] = value.Interface()
+		}
+	}
+	return result
 }
--- treflect/reflect_test.go
+++ treflect/reflect_test.go
@@ -1,8 +1,9 @@
 package treflect
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetFieldValue(t *testing.T) {
@@ -47,4 +48,18 @@ func TestSetField(t *testing.T) {
 	err = SetField(&v, "Address", "address_two")
 	assert.Nil(t, err)
 	assert.Equal(t, *v.Address, "address_two")
+}
+
+func TestToAnyMap(t *testing.T) {
+	type V struct {
+		Name    string  `json:"name,omitempty"`
+		Address *string `json:"address"`
+	}
+	address := "address"
+	v := V{
+		Name:    "name",
+		Address: &address,
+	}
+	m := ToAnyMap(v)
+	t.Logf("%+v", m)
 }
