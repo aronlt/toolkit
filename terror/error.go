1689dc1964fbf2eda8ce2b4dc77214a7ee0c3163
lintong <lintong.0825@bytedance.com>
2023-09-28 11:40:56 +0800

    add error warp

--- terror/error.go
+++ terror/error.go
@@ -8,12 +8,17 @@ import (
 )
 
 // Wrap 封装错误信息，获取错误的堆栈数据
-func Wrap(err error) error {
+func Wrap(err error, messages ...string) error {
 	pc, file, lineNo, ok := runtime.Caller(1)
 	if !ok {
 		return errors.WithMessage(err, "call runtime Caller fail")
 	}
 	funcName := runtime.FuncForPC(pc).Name()
-	info := fmt.Sprintf("file:%s:%d, function:%s\n", file, lineNo, funcName)
+	info := fmt.Sprintf("file:%s:%d, function:%s", file, lineNo, funcName)
+	if len(messages) != 0 {
+		info += " ,message:" + messages[0] + "\n"
+	} else {
+		info += "\n"
+	}
 	return errors.WithMessage(err, info)
 }
